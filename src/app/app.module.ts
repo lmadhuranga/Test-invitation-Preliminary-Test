dae49a9bbde638236a5992cd20f9de4b4548de01
Madhuranga Senadheera <madhuranga.senadeera@softlogiclife.lk>
2022-11-20 22:45:19 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyGalaryComponent } from './my-galary/my-galary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyGalaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/my-galary/my-galary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-galary',
+  templateUrl: './my-galary.component.html',
+  styleUrls: ['./my-galary.component.css']
+})
+export class MyGalaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
